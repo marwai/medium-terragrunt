85e8a5c3c7310f6233bb86dfb026d0d150003a45
Marcus Tse <marcus.tse@and.digital>
2021-12-10 11:30:08 +0000

    format

--- Medium/eu-west-1/infrastructure/load-balancer/external-alb/providers.tf
+++ Medium/eu-west-1/infrastructure/load-balancer/external-alb/providers.tf
@@ -1,5 +1,14 @@
 provider "aws" {
   region = "eu-west-1"
+  default_tags {
+    tags = {
+      Service     = "external-alb"
+      Owner       = "Marcus"
+      Contact     = "marcus.tse"
+      Project     = "Terragrunt Medium"
+      Environment = "Development"
+    }
+  }
 }
 
 terraform {
